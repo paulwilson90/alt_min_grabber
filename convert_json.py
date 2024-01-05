384362d83d1ca86a88869b9388135476110460b7
Paul <paulwilson_90@outlook.com>
2024-01-05 15:56:08 +1100

    changes to collection data and code run format. Run from main file now

--- convert_json.py
+++ convert_json.py
@@ -1,24 +1,21 @@
 import json
 
-# Read data from the text file
-with open('alternate_list.txt', "r") as file:
-    lines = file.readlines()
+def convert_to_json():
+    with open('alternate_list.txt', "r") as file:
+        lines = file.readlines()
 
-# Process the lines to create a dictionary
-destinations_data = {}
-current_destination = None
-current_letter = None
+    destinations_data = {}
 
-for line in lines:
-    line = (line.strip()).split(")")
-    current_destination = line[0][1:]
-    minima = json.loads(line[1])
-    if current_destination not in destinations_data:
-        destinations_data[current_destination] = []
+    for line in lines:
+        line = (line.strip()).split(")")
+        current_destination = line[0][1:]
+        minima = json.loads(line[1])
+        if current_destination not in destinations_data:
+            destinations_data[current_destination] = []
 
-    destinations_data[current_destination] += minima
+        destinations_data[current_destination] += minima
 
-# Create the final data dictionary
-data = destinations_data
-with open("alternate_json.json", "w") as a:
-    a.write(json.dumps(data))
+
+    with open("alternate_json.json", "w") as a:
+        a.write(json.dumps(destinations_data))
+
